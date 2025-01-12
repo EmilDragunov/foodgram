803af36552532ce8318e323ec97a0fedc904202d
Emil Dragunov <nowruzov.e@yandex.ru>
2025-01-12 21:33:07 +0300

    up

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -1,7 +1,10 @@
+"""Приложение рецептов."""
 from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
+    """Рецепты."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
     verbose_name = 'Рецепты'
--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,7 +1,10 @@
+"""Приложение пользователей."""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Пользователи."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
     verbose_name = 'Пользователи'
