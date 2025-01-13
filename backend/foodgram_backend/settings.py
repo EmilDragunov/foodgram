934fe2f58a589d1439bf60b1b9f603626d1ffe4d
Emil Dragunov <nowruzov.e@yandex.ru>
2025-01-13 21:08:01 +0300

    up

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.UserProfile'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
