2eef6da2729aeccb7f0ab170309e069d1e400854
Emil Dragunov <nowruzov.e@yandex.ru>
2025-01-17 20:56:20 +0300

    up

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,6 +7,4 @@ class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Получение объекта."""
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return request.method in SAFE_METHODS or obj.author == request.user
